0a95c1bc086a0695e71f940ffad3d637e70ebd9a
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-12-04 23:00:03 +0100

    add mongo to endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,22 +1,41 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { userSignBody } from '../types/userTypes';
+import { UserModel, User } from '../models/User';
 import jwt from 'jsonwebtoken';
+import DatabaseService from '../services/DatabaseService';
 
-export const signUser = (req: Request, res: Response, next: NextFunction) => {
+export const signUser = async (req: Request, res: Response, next: NextFunction) => {
     const userData: userSignBody = req.body;
 
-    if (userData.password === 'admin' && userData.email === 'admin@mail.com') {
-        const signToken: string = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET as string);
+    const databaseService = new DatabaseService();
+    await databaseService.connect();
+
+    try {
+        console.log(userData.email, userData.password);
+        const user: User | null = await UserModel.findOne({
+            email: userData.email,
+            password: userData.password,
+        });
+
+        if (!user) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({
+                message: 'user does not exist',
+            });
+        }
+
+        const userDataToHash = { email: user.email, password: user.password };
+
+        const signToken: string = jwt.sign(userDataToHash, process.env.ACCESS_TOKEN_SECRET as string);
 
         return res.cookie('authToken', signToken).status(StatusCodes.OK).send({
             message: 'OK',
         });
+    } catch (error) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            message: 'user does not exists',
+        });
     }
-
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-        message: 'user does not exists',
-    });
 };
 
 export const logoutUser = (req: Request, res: Response, next: NextFunction) => {
