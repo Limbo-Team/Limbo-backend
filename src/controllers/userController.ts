9e49811127d5acfef4a08cd6e7fe2c1c6da546d0
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-12-04 20:18:06 +0100

    checkpoint commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,8 +6,6 @@ import jwt from 'jsonwebtoken';
 export const signUser = (req: Request, res: Response, next: NextFunction) => {
     const userData: userSignBody = req.body;
 
-    console.log('cookies', req.cookies);
-
     if (userData.password === 'admin' && userData.email === 'admin@mail.com') {
         const signToken: string = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET as string);
 
